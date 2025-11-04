e153c51dfcb5cc3a662ce0a79b9734af46333784
KimYoonMin <dbsalszz@naver.com>
2025-11-04 22:02:41 +0900

    initail commit

---
+++ src/test/java/com/main/kyhspringboot/KyhSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.kyhspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KyhSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
