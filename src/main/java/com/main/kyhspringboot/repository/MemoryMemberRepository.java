e781341401e5151d3bdf4b33d39e5b1a523c8efd
KimYoonMin <dbsalszz@naver.com>
2025-11-05 20:51:15 +0900

    MemoryMemberRepository 생성

---
+++ src/main/java/com/main/kyhspringboot/repository/MemoryMemberRepository.java
@@ -0,0 +1,36 @@
+package com.main.kyhspringboot.repository;
+
+import com.main.kyhspringboot.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();
+
+    private static long sequence = 0L;
+
+    @Override
+
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.of(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(x -> x.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+}
